d2cd32dbf9317d9d01f81aa3dafa48f7341ab4d3
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2020-12-01 16:41:31 -0800

    Uses the @kiwi/ui component library, Heading1 component.

--- packages/kiwi-web/src/App.js
+++ packages/kiwi-web/src/App.js
@@ -1,7 +1,12 @@
 import React from "react"
+import { Heading1 } from "@kiwi/ui"
 
 function App() {
-  return <div>@kiwi/web</div>
+  return (
+    <div>
+      <Heading1>@kiwi/web</Heading1>
+    </div>
+  )
 }
 
 export default App
