791b2bb99cea4bd2e4b88d8d4c4ed7bb57be4bc3
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2020-12-01 16:06:07 -0800

    Webpack dev config, with template React files.
    https is configured in ./.webpack/webpack.config.js, place files in ./.webpack/ssl.

---
+++ packages/kiwi-web/.webpack/webpack.config.js
@@ -0,0 +1,52 @@
+// development
+const path = require("path")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { ESBuildPlugin } = require("esbuild-loader")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].[contenthash].bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "esbuild-loader",
+        options: {
+          loader: "jsx",
+          target: "es2015"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      }
+    ]
+  },
+  devtool: "eval-cheap-source-map",
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    https: true,
+    key: path.resolve(__dirname, "ssl/localhost.key"),
+    cert: path.resolve(__dirname, "ssl/localhost.crt")
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    }),
+    new ESBuildPlugin()
+  ]
+}
---
+++ packages/kiwi-web/src/App.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function App() {
+  return <div>@kiwi/web</div>
+}
+
+export default App
