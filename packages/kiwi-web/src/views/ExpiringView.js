1a6a6102873ae3fe2b5e52c5e1b3db73ce27fea2
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2020-12-03 21:29:17 -0800

    Adds styled components, heading component, normalize.css

--- packages/kiwi-web/src/views/ExpiringView.js
+++ packages/kiwi-web/src/views/ExpiringView.js
@@ -1,7 +1,18 @@
+import { Header } from "@kiwi/ui"
+import * as PropTypes from "prop-types"
 import React from "react"
 
-function ExpiringView() {
-  return <div>ExpiringView</div>
+function ExpiringView({ children }) {
+  return (
+    <div>
+      <Header title={"test"} />
+      expiring view
+    </div>
+  )
+}
+
+ExpiringView.propTypes = {
+  children: PropTypes.node.isRequired
 }
 
 export default ExpiringView
