8b9905933269fddb99b8313556937a71f48f5bc9
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2020-12-29 12:46:46 -0800

    feat/apteryx-http: Create handler for loading middleware.

--- packages/apteryx-http/src/index.ts
+++ packages/apteryx-http/src/index.ts
@@ -2,20 +2,33 @@ import cors from "cors"
 import express, { Application } from "express"
 import bodyParser from "body-parser"
 
+type MiddlewareTypedef = {
+  path: string
+  middleware: any
+}
+
 export default class HTTPServer {
   readonly app: Application = express()
   private readonly port: number
 
-  constructor(port: number) {
+  constructor(port: number, middleware?: MiddlewareTypedef[]) {
     this.port = port
     this.app.use(cors())
     this.app.use(bodyParser.json())
+
+    if (middleware) this.loadMiddleware(middleware)
   }
 
-  public listen(): HTTPServer {
+  public listen(): this {
     this.app.listen(this.port, (): void => {
       console.log(`Listening on port ${this.port}`)
     })
     return this
+  }
+
+  private loadMiddleware(middleware: MiddlewareTypedef[]) {
+    for (const middlewareKey of middleware) {
+      this.app.use(middlewareKey.path, middlewareKey.middleware)
+    }
   }
 }
