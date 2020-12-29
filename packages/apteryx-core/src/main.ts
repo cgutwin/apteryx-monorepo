8ae20fa672b2cc8596e9532bac6a944a6b76aa27
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2020-12-29 12:48:52 -0800

    feat/apteryx-core: Implement @apteryx/graphql.

--- packages/apteryx-core/src/main.ts
+++ packages/apteryx-core/src/main.ts
@@ -1,5 +1,12 @@
+import GraphQLServer from "@apteryx/graphql"
 import HTTPServer from "@apteryx/http"
 
-const server = new HTTPServer(4000)
+const graphQlServer = new GraphQLServer(true).loadServer()
+const server = new HTTPServer(4000, [
+  {
+    path: "/",
+    middleware: graphQlServer
+  }
+])
 
 export { server }
