da5a4da990aa38ddf01f082c7db121248368a41a
Chris Gutwin <24538491+cgutwin@users.noreply.github.com>
2021-01-06 11:52:23 -0800

    Adds CORS config, restricting to a single origin based off env.

--- packages/apteryx/src/http.ts
+++ packages/apteryx/src/http.ts
@@ -1,4 +1,4 @@
-import cors from "cors"
+import cors, { CorsOptions } from "cors"
 import express, { Application } from "express"
 import bodyParser from "body-parser"
 
@@ -11,9 +11,10 @@ export default class HTTPServer {
   readonly app: Application = express()
   private readonly port: number
 
-  constructor(port: number, middleware?: MiddlewareTypedef[]) {
+  constructor(port: number, middleware?: MiddlewareTypedef[], corsOptions?: CorsOptions | undefined) {
     this.port = port
-    this.app.use(cors())
+    console.log(`Using CORS options ${JSON.stringify(corsOptions)}`)
+    this.app.use(cors(corsOptions))
     this.app.use(bodyParser.json())
 
     if (middleware) this.loadMiddleware(middleware)
--- packages/apteryx/src/main.ts
+++ packages/apteryx/src/main.ts
@@ -1,12 +1,19 @@
 import GraphQLServer from "./graphql/server"
 import HTTPServer from "./http"
 
+const port = (process.env.PORT as unknown) as number
 const graphQlServer = new GraphQLServer(true).loadServer()
-const server = new HTTPServer(4000, [
+const server = new HTTPServer(
+  port,
+  [
+    {
+      path: "/",
+      middleware: graphQlServer
+    }
+  ],
   {
-    path: "/",
-    middleware: graphQlServer
+    origin: process.env.CORS_ORIGIN
   }
-])
+)
 
 export { server }
